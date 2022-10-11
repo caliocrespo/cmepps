6bf0abbf79bf1750dca62025da720b53229486a1
usuario <usuario@pc22-036>
2022-10-11 10:22:14 +0200

    calculadora

---
+++ Sesion2/src/Calculadora.java
@@ -0,0 +1,23 @@
+
+public class Calculadora {
+
+	public int sumar(int a, int b) {
+		return a+b;
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+	
+	public int restar(int a, int b) {
+		return a-b;
+	}
+	
+	public int dividir(int a, int b) {
+		if(b==0)
+			return -1;
+		else
+		
+			return a+b;
+	}
+}
